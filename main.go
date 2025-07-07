bd9e6f66a4c04abab426f57be0ebeeda31fadbeb
Kam <erkamyaman35@gmail.com>
2025-07-07 23:12:15 +0300

    if-else

--- main.go
+++ main.go
@@ -14,6 +14,18 @@ func main() {
     age := 25
     fmt.Printf("=== Printf ===\nMy name is %s and I’m %d years old.\n", name, age)
 	fmt.Println(name2)
+
+	var score = 75
+
+if score >= 90 {
+    fmt.Println("A")
+} else if score >= 80 {
+    fmt.Println("B")
+} else if score >= 70 {
+    fmt.Println("C")
+} else {
+    fmt.Println("F")
+}
 }
 
 var name2 = "yaman"
@@ -22,3 +34,5 @@ var isActive bool      // false
 var points int         // 0
 var note string        // ""
 var rating float64     // 0.0
+
+
