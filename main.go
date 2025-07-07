4dc0c78c4e29e81a4ac217e7fbbafd0c5d3c01bb
Kam <erkamyaman35@gmail.com>
2025-07-07 23:06:58 +0300

    variables

--- main.go
+++ main.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	// line by line
     fmt.Println("Hello, Go!")
+	// same line
+	fmt.Print("Hello, ")
+	fmt.Print("world!")
 	help()
+
+	name := "Erkam"
+    age := 25
+    fmt.Printf("=== Printf ===\nMy name is %s and I’m %d years old.\n", name, age)
+	fmt.Println(name2)
 }
+
+var name2 = "yaman"
+
+var isActive bool      // false
+var points int         // 0
+var note string        // ""
+var rating float64     // 0.0
